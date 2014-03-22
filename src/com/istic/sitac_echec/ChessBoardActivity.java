f1fe9321d17b55cabb21afb75eb92b403b3bb585
vincent <vincent.bodiou@gmail.com>
2014-03-22 18:29:34 +0100

    menu

--- src/com/istic/sitac_echec/ChessBoardActivity.java
+++ src/com/istic/sitac_echec/ChessBoardActivity.java
@@ -1,25 +1,15 @@
 package com.istic.sitac_echec;
 
 import android.os.Bundle;
-import android.view.Menu;
-import com.istic.sit.R;
-
-import com.istic.sit.framework.view.ChessBoard;
+import com.istic.sit.framework.view.MainActivity;
 
 
-public class ChessBoardActivity extends ChessBoard {
+public class ChessBoardActivity extends MainActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.chess_board);
 		
+		initializeBackground(TypeBackgroundEnum.Board);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
